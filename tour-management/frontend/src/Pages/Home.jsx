0a5b56f8bb9f0f47feb521a33ce914d35fe01971
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-05 01:07:24 +0530

    update

--- tour-management/frontend/src/Pages/Home.jsx
+++ tour-management/frontend/src/Pages/Home.jsx
@@ -12,47 +12,60 @@ import Searchbar from "../Shared/Searchbar";
 const Home = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col lg="6">
-            <div className="hero_content">
-              <div className="hero__subtitle d-flex align-items-center">
-                <Subtitle Subtitle={"Know Before You Go"} />
-                <img src={worldImg} alt="" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero_content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle Subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+                  delectus iure dolores, dignissimos unde omnis quae eligendi
+                  veniam dolorem neque nobis sit enim repellat! Repellendus
+                  tempora rem vitae aperiam facilis.
+                </p>
               </div>
-              <h1>
-                Traveling opens the door to creating
-                <span className="highlight">memories</span>
-              </h1>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                delectus iure dolores, dignissimos unde omnis quae eligendi
-                veniam dolorem neque nobis sit enim repellat! Repellendus
-                tempora rem vitae aperiam facilis.
-              </p>
-            </div>
-          </Col>
+            </Col>
 
-          <Col lg="2">
-            <div className="hero__img-box">
-              <img src={heroImg} alt="" />
-            </div>
-          </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
 
-          <Col lg="2">
-            <div className="hero__img-box mt-4">
-              <video src={heroVideo} alt="" controls />
-            </div>
-          </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
 
-          <Col lg="2">
-            <div className="hero__img-box mt-5">
-              <img src={heroImg02} alt="" controls />
-            </div>
-          </Col>
-          <Searchbar />
-        </Row>
-      </Container>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" controls />
+              </div>
+            </Col>
+            <Searchbar />
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h1 className="services__title">We offer our best services</h1>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
