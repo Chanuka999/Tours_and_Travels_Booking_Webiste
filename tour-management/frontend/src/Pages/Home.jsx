15701e809d5f188adde93a54c262eb6eb7ed81d7
IsuruSampathRanasinghe <isuruep1@gmail.com>
2025-05-17 11:49:59 +0530

    Update

--- tour-management/frontend/src/Pages/Home.jsx
+++ tour-management/frontend/src/Pages/Home.jsx
@@ -70,6 +70,18 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className="mb-5">
+              <Subtitle subtitle={'Explore'}/>
+              <h2 className="featured_tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList/>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
