b335911e834f4cc0a06d00d6590d465312d04562
Chanuka999 <chanukaranditha99@gmail.com>
2025-06-05 23:51:13 +0530

    refactor: update Tours component for improved data fetching and code consistency

--- tour-management/frontend/src/Pages/Tour.jsx
+++ tour-management/frontend/src/Pages/Tour.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
-import CommonSection from "../Shared/CommonSection";
-import { Container, Row, Col } from "react-bootstrap";
-
+import CommonSection from "./../Shared/CommonSection";
+// import tourData from '../assets/data/tours'
 import "../styles/tour.css";
-import tourData from "../assest/data/tours";
 import TourCard from "./../Shared/TourCard";
-import Searchbar from "./../Shared/Searchbar";
+import SearchBar from "./../Shared/Searchbar";
 import Newsletter from "./../Shared/Newsletter";
-//import { useEffect } from "react";
+import { Col, Container, Row } from "reactstrap";
+import useFetch from "../hooks/useFetch";
+import { BASE_URL } from "../utils/config";
 
-const Tour = () => {
+const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
+  const {
+    data: tours,
+    loading,
+    error,
+  } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
+
   useEffect(() => {
-    const pages = Math.ceil(5 / 4);
+    const pages = Math.ceil(tourCount / 8);
     setPageCount(pages);
-  }, [page]);
+    window.scrollTo(0, 0);
+  }, [page, tourCount, tours]);
 
   return (
     <>
@@ -24,36 +32,39 @@ const Tour = () => {
       <section>
         <Container>
           <Row>
-            <Searchbar />
+            <SearchBar />
           </Row>
         </Container>
       </section>
+
       <section className="pt-0">
         <Container>
-          <Row>
-            {tourData?.map((tour) => (
-              <Col lg="3" className="mb-4" key={tour.id}>
-                <TourCard tour={tour} />
-              </Col>
-            ))}
+          {loading && <h4 className="text-center pt-5">LOADING..........</h4>}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
+          {!loading && !error && (
+            <Row>
+              {tours?.map((tour) => (
+                <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+                  {" "}
+                  <TourCard tour={tour} />{" "}
+                </Col>
+              ))}
 
-            <Col lg="12">
-              <div
-                className="pagination d-flex align-items-center
-          justify-content-center mt-4 gap-3"
-              >
-                {[...Array(pageCount).keys()].map((number) => (
-                  <span
-                    key={number}
-                    onClick={() => setPage(number)}
-                    className={page === number ? "active__page" : ""}
-                  >
-                    {number + 1}
-                  </span>
-                ))}
-              </div>
-            </Col>
-          </Row>
+              <Col lg="12">
+                <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                  {[...Array(pageCount).keys()].map((number) => (
+                    <span
+                      key={number}
+                      onClick={() => setPage(number)}
+                      className={page === number ? "active__page" : ""}
+                    >
+                      {number + 1}
+                    </span>
+                  ))}
+                </div>
+              </Col>
+            </Row>
+          )}
         </Container>
       </section>
       <Newsletter />
@@ -61,4 +72,4 @@ const Tour = () => {
   );
 };
 
-export default Tour;
+export default Tours;
